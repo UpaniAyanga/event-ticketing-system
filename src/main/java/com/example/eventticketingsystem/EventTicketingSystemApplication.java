2efa72441471c6f6a7fe8f37257ca9f4b203deaf
UpaniAyanga <upanilokusuriya@gmail.com>
2024-10-29 00:13:27 +0530

    Initial Commit

---
+++ src/main/java/com/example/eventticketingsystem/EventTicketingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.eventticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(EventTicketingSystemApplication.class, args);
+	}
+
+}
